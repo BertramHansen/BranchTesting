475d22f074ac69bfb2c4df7ace872a59d6e3c54b
BertramHansen <beha0007@stud.kea.dk>
2024-11-21 13:14:09 +0100

    added Main class with runnable main code

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal hund = new Dog("Fiddo");
+        Animal kat = new Cat("Sofus");
+
+
+        System.out.println("Dog says:");
+        hund.makeSound();
+
+        System.out.println("Cat says:");
+        kat.makeSound();
+
+    }
+
+}
