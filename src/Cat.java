4059429d98e4924cd8b4b1113a834ed9aae4e5a3
BertramHansen <beha0007@stud.kea.dk>
2024-11-21 12:56:56 +0100

    MakeSound implemented

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,11 @@
 public class Cat extends Animal{
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("meow");
+    }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,11 @@
 public class Dog extends Animal{
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("woof");
+    }
 }
